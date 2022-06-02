4274e39912469ae4e38a3ab7d7eaa0f08bbb2b5f
Adylan Roaffa <adylan.dev@gmail.com>
2022-06-02 23:05:41 +0800

    edit placeholder photo

--- components/sections/MainSection.tsx
+++ components/sections/MainSection.tsx
@@ -3,14 +3,13 @@ import CoupleGeneralInfo from '../CoupleGeneralInfo'
 import QuranVerse from '../QuranVerse'
 
 interface Props {
-
 }
 
 export default function MainSection({ }: Props): ReactElement {
     return (
         <div className='flex flex-col'>
             <div className="mt-10">
-                <CoupleGeneralInfo imageSrc="https://picsum.photos/356/356" />
+                <CoupleGeneralInfo imageSrc="https://via.placeholder.com/356x356?text=Photo+Adylan+Azka" />
             </div>
             <div className=''>
                 <QuranVerse />
